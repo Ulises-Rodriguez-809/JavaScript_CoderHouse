30b535425a4bc148d3be00e7806ddc1f7a93deab
Ulises Rodriguez <uliisesrodriguez809@gmail.com>
2023-08-10 17:34:13 -0300

    login terminado

--- js/equipos.js
+++ js/equipos.js
@@ -19,96 +19,51 @@ export const noCoffeNoWorkee = [
     },
     {
         jugador: new Jugadores("Ludmi", 4, "noCoffeNoWorkee")
-
     },
     {
         jugador: new Jugadores("Fede", 5, "noCoffeNoWorkee")
-
     },
     {
         jugador: new Jugadores("Fernando", 2, "noCoffeNoWorkee")
-
     },
     {
         jugador: new Jugadores("Maria", 1, "noCoffeNoWorkee")
-
     },
     {
         jugador: new Jugadores("Luciana", 8, "noCoffeNoWorkee")
-
     },
     {
         jugador: new Jugadores("Luciano", 8, "noCoffeNoWorkee")
-
     },
     {
         jugador: new Jugadores("Juan", 10, "noCoffeNoWorkee")
-        // nombre: "Juan",
-        // puntuacion: 10,
-        // equipo: "noCoffeNoWorkee"
     },
     {
         jugador: new Jugadores("Facu", 10, "noCoffeNoWorkee")
-
-        // nombre: "Facu",
-        // puntuacion: 10,
-        // equipo: "noCoffeNoWorkee"
     },
     {
         jugador: new Jugadores("Martin", 9, "noCoffeNoWorkee")
-
-        // nombre: "Martin",
-        // puntuacion: 9,
-        // equipo: "noCoffeNoWorkee"
     },
     {
         jugador: new Jugadores("Lucho", 7, "noCoffeNoWorkee")
-
-        // nombre: "Lucho",
-        // puntuacion: 7,
-        // equipo: "noCoffeNoWorkee"
     },
     {
         jugador: new Jugadores("Yo", 7, "noCoffeNoWorkee")
-
-        // nombre: "Yo",
-        // puntuacion: 7,
-        // equipo: "noCoffeNoWorkee"
     },
     {
         jugador: new Jugadores("Pepe", 6, "noCoffeNoWorkee")
-
-        // nombre: "Pepe",
-        // puntuacion: 6,
-        // equipo: "noCoffeNoWorkee"
     },
     {
         jugador: new Jugadores("ElMagician", 5, "noCoffeNoWorkee")
-
-        // nombre: "ElMagician",
-        // puntuacion: 5,
-        // equipo: "noCoffeNoWorkee"
     },
     {
         jugador: new Jugadores("Lenox", 3, "noCoffeNoWorkee")
-
-        // nombre: "Lenox",
-        // puntuacion: 3,
-        // equipo: "noCoffeNoWorkee"
     },
     {
         jugador: new Jugadores("ElPampa", 3, "noCoffeNoWorkee")
-
-        // nombre: "ElPampa",
-        // puntuacion: 3,
-        // equipo: "noCoffeNoWorkee"
     },
     {
         jugador: new Jugadores("Tatun", 3, "noCoffeNoWorkee")
-
-        // nombre: "Tatun",
-        // puntuacion: 3,
-        // equipo: "noCoffeNoWorkee"
     }
 ]
 
@@ -153,39 +108,39 @@ export const codigoYCafe = [
 
 export const iTurnCoffeIntoCode = [
     {
-        jugador: new Jugadores("Javier", 7, "codigoYCafe")
+        jugador: new Jugadores("Javier", 7, "iTurnCoffeIntoCode")
     },
     {
-        jugador: new Jugadores("Coco", 10, "codigoYCafe")
+        jugador: new Jugadores("Coco", 10, "iTurnCoffeIntoCode")
     },
     {
-        jugador: new Jugadores("Laura", 10, "codigoYCafe")
+        jugador: new Jugadores("Laura", 10, "iTurnCoffeIntoCode")
     },
     {
-        jugador: new Jugadores("Alicia", 9, "codigoYCafe")
+        jugador: new Jugadores("Alicia", 9, "iTurnCoffeIntoCode")
     },
     {
-        jugador: new Jugadores("Bibi", 2, "codigoYCafe")
+        jugador: new Jugadores("Bibi", 2, "iTurnCoffeIntoCode")
     },
     {
-        jugador: new Jugadores("Joshep", 3, "codigoYCafe")
+        jugador: new Jugadores("Joshep", 3, "iTurnCoffeIntoCode")
     },
     {
-        jugador: new Jugadores("Lucy", 6, "codigoYCafe")
+        jugador: new Jugadores("Lucy", 6, "iTurnCoffeIntoCode")
     },
     {
-        jugador: new Jugadores("Maximiliano", 1, "codigoYCafe")
+        jugador: new Jugadores("Maximiliano", 1, "iTurnCoffeIntoCode")
     },
     {
-        jugador: new Jugadores("Marcelo", 5, "codigoYCafe")
+        jugador: new Jugadores("Marcelo", 5, "iTurnCoffeIntoCode")
     },
     {
-        jugador: new Jugadores("Rocio", 8, "codigoYCafe")
+        jugador: new Jugadores("Rocio", 8, "iTurnCoffeIntoCode")
     },
     {
-        jugador: new Jugadores("Martincho", 10, "codigoYCafe")
+        jugador: new Jugadores("Martincho", 10, "iTurnCoffeIntoCode")
     },
     {
-        jugador: new Jugadores("Juanchis", 10, "codigoYCafe")
+        jugador: new Jugadores("Juanchis", 10, "iTurnCoffeIntoCode")
     }
 ]
--- js/login.js
+++ js/login.js
@@ -1,7 +1,7 @@
 const usuariosRegistrados = [
     {
         usuario: 'Ulises',
-        contraseña: '12345'
+        contraseña: '12345',
     },
     {
         usuario: 'Tino',
@@ -20,27 +20,89 @@ let usuarioReg = {
     ConfirmarCont: ""
 }
 
-
-const opcion = ()=>{
+const opcion = () => {
     const btnOpcion__iniciarSesion = document.getElementById("btnOpcion__iniciarSesion");
     const btnOpcion__registrarse = document.getElementById("btnOpcion__registrarse");
 
+    //forma 1
+    // const login = document.getElementById("login");
+
+    //forma 2
     const iniciarSesion = document.getElementById("iniciarSesion");
     const registrarse = document.getElementById("registrarse");
 
-    btnOpcion__iniciarSesion.addEventListener("click",()=>{
+    btnOpcion__iniciarSesion.addEventListener("click", () => {
 
+        //forma 1 
+        // en ves de tener tanto el apartado de iniciar sesion como el de registrarse en el html 
+        //dejo 1 de los 2
+        //y luego lo voy creando dependiendo del btn q se pulsa
+        // login.innerHTML = `
+        // <div id="iniciarSesion">
+        //     <div id="logTitulo">
+        //         <p>Iniciar sesion</p>
+        //     </div>
+        //     <div id="logDatos">
+        //         <div>
+        //             <label for="logNombre">Nombre</label>
+        //             <input type="text" name="usuario" id="logNombre" class="inputLog"
+        //                 placeholder="Escriba su Nombre">
+        //         </div>
+        //         <div>
+        //             <label for="logContraseña">Contraseña</label>
+        //             <input type="password" name="contraseña" id="logContraseña" class="inputLog"
+        //                 placeholder="Escriba su contraseña">
+        //         </div>
+        //     </div>
+        //     <div class="msg msgInc"></div>
+        //     <div class="btnLogContainer">
+        //         <button id="btnLog">Iniciar sesion</button>
+        //     </div>
+        // </div>
+        // `;
+
+        //la otra forma de hacerlo es dejando todo estatico en el html y cambiar el display
         iniciarSesion.style.display = "flex";
         registrarse.style.display = "none";
+
+        //la diferencia entre forma 1 y 2 es q forma_1 cuando te inicias sesion te muestra un mensaje pero si volves a tocar el btn de iniciar sesion te muestra todo el formulario devuelta
+        //miesntras q la forma_2 una ves iniciado sesion por mas q toques el btn iniciar sesion sigue mostrando el mensaje de q ya iniciaste sesion
     })
 
-    btnOpcion__registrarse.addEventListener("click",()=>{
+    btnOpcion__registrarse.addEventListener("click", () => {
+
+        // login.innerHTML = `
+        // <div id="registrarse">
+        //     <div id="regTitulo">
+        //         <p>Registrarse</p>
+        //     </div>
+        //     <div id="regDatos">
+        //         <div>
+        //             <label for="regNombre">Nombre</label>
+        //             <input type="text" name="usuario" id="regNombre" class="inputReg"
+        //                 placeholder="Escriba su Nombre">
+        //         </div>
+        //         <div>
+        //             <label for="regContraseña">Contraseña</label>
+        //             <input type="password" name="contraseña" id="regContraseña" class="inputReg"
+        //                 placeholder="Escriba su contraseña">
+        //         </div>
+        //         <div>
+        //             <label for="regConConfirmar">Confirmar Contraseña</label>
+        //             <input type="password" name="ConfirmarCont" id="regConConfirmar" class="inputReg"
+        //                 placeholder="Confirmar Contraseña">
+        //         </div>
+        //     </div>
+        //     <div class="btnRegContainer">
+        //         <button id="btnReg">Registrarse</button>
+        //     </div>
+        // </div>
+        // `;
 
         iniciarSesion.style.display = "none";
         registrarse.style.display = "flex";
     })
 }
-
 
 const cargarStorage = (arr) => {
     //cargamos al local storage algunos usuarios 
@@ -72,14 +134,15 @@ const capturarValores = (arr, nombreObjeto) => {
                         ...usuarioReg,
                         [name]: value
                     }
+
+                    console.log(usuarioReg)
+
                     break;
                 default:
                     break;
             }
         })
     })
-
-    // return objeto;
 }
 
 const datosIn = () => {
@@ -87,34 +150,30 @@ const datosIn = () => {
     capturarValores(inputDatos, "usuarioIn");
 }
 
+const datosReg = ()=>{
+    const inputDatos = document.querySelectorAll(".inputReg");
+    capturarValores(inputDatos, "usuarioReg");
+}
+
 const casoLogin = () => {
-    const login = document.getElementById("login");
+    const logDatos = document.getElementById("logDatos");
     const msg = document.querySelector(".msg");
     const btnLog = document.getElementById("btnLog");
 
     //obtenemos el contenido guardado en el localStorage para comparar
-    debugger
-    const arrUsuarios = JSON.parse(localStorage.getItem("usuarios"));
+    let arrUsuarios = JSON.parse(localStorage.getItem("usuarios"));
 
     btnLog.addEventListener("click", () => {
-        debugger
-        console.log(usuarioIn)
+
         //se q podria usar directamente usuariosRegistrados en ves de arrUsuarios y ahorrarme hacer todo le json parse,etc pero es para poder practicar mas
-        let usuarioEncontrado = arrUsuarios.find((element) => {
-            debugger
-            element.usuario === usuarioIn.usuario && element.contraseña === usuarioIn.contraseña
-        });
+        let usuarioEncontrado = arrUsuarios.find((element) => element.usuario === usuarioIn.usuario && element.contraseña === usuarioIn.contraseña);
 
         if (usuarioEncontrado) {
-            login.innerHTML = `
-                <div id="logTitulo">
-                        <p>Iniciar sesion</p>
-                </div>
-                <div class="msg msgInc"></div>
-            `;
+
+            logDatos.innerHTML = "";
 
             msg.innerHTML = `
-                Se inicio secion con exito
+                <p>Se inicio secion con exito</p>
                 <a href="./pages/quiz.html" class="link">Ir al juego</a>
             `;
 
@@ -133,26 +192,67 @@ const casoLogin = () => {
     })
 }
 
-const comprobarSiRegistrado = (aux) => {
+const casoReg = () => {
+    const regDatos = document.getElementById("regDatos");
+    const msg2 = document.querySelector(".msg2");
+    const btnReg = document.getElementById("btnReg");
 
-    casoLogin(aux);
+    let arrUsuarios = JSON.parse(localStorage.getItem("usuarios"));
 
+    btnReg.addEventListener("click",()=>{
 
+        let usuarioEncontrado = arrUsuarios.find((element) => element.usuario === usuarioReg.usuario);
 
+        const regContraseña = document.getElementById("regContraseña");
+        const regConConfirmar = document.getElementById("regConConfirmar");
 
-    // if (estaRegistrado) {
-    //     //lo redireccionamos al quiz 
+        if (usuarioEncontrado) {
+            msg2.innerHTML = `
+                <p>Ese usuario ya existe</p>
+            `;
 
-    // }else{
-    //     //le mostramos en el DOM q no puede iniciar sesion xq no esta registrado
-    //     //le pedimos q se registre
-    // }
+            msg2.classList.replace("msgCor", "msgInc");
+            msg2.style.display = "block";
 
+        }
+        else if (regContraseña.value !== regConConfirmar.value) {
+            msg2.innerHTML = `
+                <p>Las contraseñas no coinciden</p>
+            `;
+
+            msg2.classList.replace("msgCor", "msgInc");
+            msg2.style.display = "block";
+        }
+        else{
+            //creamos el nuevo usuario
+            let aux = {
+                usuario : usuarioReg.usuario,
+                contraseña : usuarioReg.contraseña
+            }
+            
+            //lo pusheamos dentro del array q nos devuelve al hacer el JSON.parse(localStorage.getItem("usuarios"))
+            arrUsuarios.push(aux);
+            
+            //guardamos los cambios en el local storage
+            //sin olvidar convertir a texto el arrUsuarios ya q los arrays y objetos si no los convertis a texto no se guardan bien en el localStorage 
+            localStorage.setItem("usuarios", JSON.stringify(arrUsuarios));
+
+            regDatos.innerHTML = "";
+
+            msg2.innerHTML = `
+                <p>Usuario registrado con exito</p>
+                <a href="./pages/quiz.html" class="link">Ir al juego</a>
+            `;
+
+            msg2.classList.replace("msgInc", "msgCor");
+            msg2.style.display = "block";
+        }
+    })
 }
 
-// let a = JSON.parse(localStorage.getItem("usuarios"));
-// console.log(a)
-
-opcion();
 cargarStorage(usuariosRegistrados);
-// comprobarSiRegistrado(aux)
+opcion();
+datosIn();
+datosReg();
+casoLogin();
+casoReg();
